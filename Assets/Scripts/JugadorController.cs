cc30a8321903ca72022b8a2c298f17aa11ad6660
RodolfoMH <joserodolfomore@gmail.com>
2020-10-17 15:23:54 -0400

    first commit

---
+++ Assets/Scripts/JugadorController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JugadorController : MonoBehaviour
+{
+    //Declarlo la variable de tipo RigidBody que luego asociaremos a nuestro Jugador
+    private Rigidbody rb;
+    public float velocidad;
+    private int contador;
+    public Text textoContador, textoGanar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        contador = 0;
+        //Actualizo el texto del contador por pimera vez
+        setTextoContador();
+        //Inicio el texto de ganar a vacío
+        textoGanar.text = "";
+
+    }
+
+    // Para que se sincronice con los frames de física del motor
+    void FixedUpdate () 
+    {
+        //Estas variables nos capturan el movimiento en horizontal y vertical de nuestro teclado
+        float movimientoH = Input.GetAxis("Horizontal");
+        float movimientoV = Input.GetAxis("Vertical");
+        //Un vector 3 es un trío de posiciones en el espacio XYZ, en este caso el que corresponde al movimiento
+        Vector3 movimiento = new Vector3(movimientoH, 0.0f, movimientoV);
+        //Asigno ese movimiento o desplazamiento a mi RigidBody
+        rb.AddForce(movimiento*velocidad);
+    }
+
+    //Se ejecuta al entrar a un objeto con la opción isTrigger seleccionada
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag ("Coleccionable"))
+        {
+            other.gameObject.SetActive (false);
+            contador = contador + 1;
+            //Actualizo el texto del contador
+            setTextoContador();
+        }
+    }
+
+    //Actualizo el texto del contador (O muestro el de ganar si las ha cogido todas)
+    void setTextoContador()
+    {
+        textoContador.text = "Contador: " + contador.ToString();
+        if (contador >= 10)
+        {
+            textoGanar.text = "¡Ganaste!";
+        }
+    }
+
+
+
+}
